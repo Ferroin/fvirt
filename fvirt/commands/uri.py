1053bb8127a1731d8b5e1d177f3b9ff94eeb2da9
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-10-02 13:07:59 -0400

    Rewrite uri command to use our custom command class.

--- fvirt/commands/uri.py
+++ fvirt/commands/uri.py
@@ -7,19 +7,31 @@ from __future__ import annotations
 
 import click
 
+from ._base.command import Command
+
 from ..libvirt import Hypervisor
 
+HELP = '''
+Print the hypervisor URI.
 
-@click.command
-@click.pass_context
-def uri(ctx: click.core.Context) -> None:
-    '''Print the hypervisor URI.
+This functions identically to the virsh `uri` command, except it uses
+the fvirt.libvirt bindings.
 
-       This functions identically to the virsh `uri` command, except it
-       uses the fvirt.libvirt bindings.'''
+For more information about how fvirt handles libvirt URIs, run `fvirt
+help connections`.
+'''.lstrip().rstrip()
+
+
+def cb(ctx: click.Context) -> None:
     with Hypervisor(hvuri=ctx.obj['uri']) as hv:
         click.echo(str(hv.uri))
 
+
+uri = Command(
+    name='uri',
+    help=HELP,
+    callback=cb,
+)
 
 __all__ = [
     'uri',
