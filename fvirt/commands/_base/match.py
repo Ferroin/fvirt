7e3707e009d7f0ff6fc270bfcebcbdee527f023d
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-10-02 21:22:22 -0400

    Merge util.match_params into commands._base.match.
    It’s the only place it’s used, so avoid an extra import.

--- fvirt/commands/_base/match.py
+++ fvirt/commands/_base/match.py
@@ -5,15 +5,16 @@
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Self, Any, ParamSpec, TypeVar, Concatenate
+from typing import TYPE_CHECKING, Self, Any, ParamSpec, Type, TypeVar, Concatenate
 
 import click
+
+from lxml import etree
 
 from .command import Command
 
 from ...libvirt.entity import Entity
 from ...util.match import MatchAlias, MatchTarget
-from ...util.match_params import MatchTargetParam, MatchPatternParam
 
 if TYPE_CHECKING:
     import re
@@ -24,6 +25,55 @@ if TYPE_CHECKING:
 
 P = ParamSpec('P')
 T = TypeVar('T')
+
+DEFAULT_MATCH = re.compile('.*')
+
+
+def MatchTargetParam(aliases: Mapping[str, MatchAlias]) -> Type[click.ParamType]:
+    '''Factory function for creating types for match tagets.
+
+       This will produce a subclass of click.ParamType for parsing the
+       first parameter that should be passed to the `--match` argument
+       and converting it to a usable MatchTarget instance, based on the
+       mapping of aliases.
+
+       The resultant class can be used with the `type` argument for
+       click.option decorators to properly parse match targets for the
+       `--match` option.'''
+    class MatchTargetParam(click.ParamType):
+        name = 'match-target'
+
+        def convert(self: Self, value: str | MatchTarget, param: Any, ctx: click.core.Context | None) -> MatchTarget:
+            if isinstance(value, str):
+                if value in aliases:
+                    ret = MatchTarget(property=aliases[value].property)
+                else:
+                    ret = MatchTarget(xpath=etree.XPath(value, smart_strings=False))
+            else:
+                ret = value
+
+            return ret
+
+    return MatchTargetParam
+
+
+class MatchPatternParam(click.ParamType):
+    '''Class for processing match patterns.
+
+       When used as a type for a Click option, this produces a re.Pattern
+       object for use with the fvirt.util.match.matcher() function.'''
+    name = 'pattern'
+
+    def convert(self: Self, value: str | re.Pattern | None, param: Any, ctx: click.core.Context | None) -> re.Pattern:
+        if isinstance(value, str):
+            try:
+                return re.compile(value)
+            except re.error:
+                self.fail(f'"{ value }" is not a valid pattern.', param, ctx)
+        elif value is None:
+            return DEFAULT_MATCH
+        else:
+            return value
 
 
 def get_match_or_entity(
