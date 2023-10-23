86aebb87dadc550df861f14fba40777fd44b0409
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-10-23 11:27:14 -0400

    Add dummy tests for individual CLI commands.
    Long-term we need to implement testing for these, but for now just add
    an xfail test for each one as a reminder.

---
+++ tests/commands/volume/commands_volume_wipe_test.py
@@ -0,0 +1,14 @@
+# Copyright (c) 2023 Austin S. Hemmelgarn
+# SPDX-License-Identifier: MITNFA
+
+'''Tests for fvirt.commands.volume.wipe'''
+
+from __future__ import annotations
+
+import pytest
+
+
+@pytest.mark.xfail(reason='Not yet implemented.')
+def test_command_run() -> None:
+    '''Test that the command runs correctly.'''
+    assert False
