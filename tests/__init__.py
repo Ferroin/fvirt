2eea2e747f59d7bcc5184e20d40b890ec4839603
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-10-23 09:36:32 -0400

    Fix assertion rewriting for tests.

---
+++ tests/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (c) 2023 Austin S. Hemmelgarn
+# SPDX-License-Identifier: MITNFA
+
+import pytest
+
+pytest.register_assert_rewrite('tests.commands.shared')
+pytest.register_assert_rewrite('tests.libvirt.shared')
