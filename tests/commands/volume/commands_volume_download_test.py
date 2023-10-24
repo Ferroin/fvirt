795a8d48aeaa8390dfa0681017f653d26bb4548c
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-10-24 09:55:38 -0400

    Add tests for volume upload and volume download commands.

--- tests/commands/volume/commands_volume_download_test.py
+++ tests/commands/volume/commands_volume_download_test.py
@@ -5,10 +5,65 @@
 
 from __future__ import annotations
 
+import filecmp
+import os
+import random
+import sys
+
+from pathlib import Path
+from typing import TYPE_CHECKING, Any
+
 import pytest
 
+from fvirt.cli import cli
 
-@pytest.mark.xfail(reason='Not yet implemented.')
-def test_command_run() -> None:
-    '''Test that the command runs correctly.'''
-    assert False
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from click.testing import CliRunner
+
+    from fvirt.libvirt import Volume
+
+
+@pytest.mark.slow
+@pytest.mark.libvirtd
+def test_volume_download(tmp_path: Path, cli_runner: CliRunner, live_volume: Volume, unique: Callable[..., Any]) -> None:
+    '''Test volume download command.'''
+    uri = str(live_volume._hv.uri)
+    pool = live_volume._parent
+    vol_path = Path(live_volume.path)
+    target_path = tmp_path / unique('text', prefix='fvirt-test')
+
+    assert pool is not None
+
+    vol_path.write_bytes(random.randbytes(live_volume.capacity))
+
+    result = cli_runner.invoke(cli, ('-c', uri, 'volume', 'download', pool.name, live_volume.name, str(target_path)))
+    assert result.exit_code == 0, result.output
+
+    assert filecmp.cmp(vol_path, target_path, shallow=False)
+
+
+@pytest.mark.slow
+@pytest.mark.libvirtd
+@pytest.mark.xfail(condition=sys.platform == 'win32', reason='Sparse data handling not supported on Windows')
+def test_volume_sparse_download(tmp_path: Path, cli_runner: CliRunner, live_volume: Volume, unique: Callable[..., Any]) -> None:
+    '''Test volume sparse download functionality.'''
+    uri = str(live_volume._hv.uri)
+    pool = live_volume._parent
+    vol_path = Path(live_volume.path)
+    target_path = tmp_path / unique('text', prefix='fvirt-test')
+    assert pool is not None
+
+    block_count = 8
+    block = live_volume.capacity // block_count
+
+    with vol_path.open('wb') as f:
+        for k in range(0, block_count // 2):
+            f.seek(block, os.SEEK_CUR)
+            f.write(random.randbytes(block))
+
+    result = cli_runner.invoke(cli, ('-c', uri, 'volume', 'download', '--sparse', pool.name, live_volume.name, str(target_path)))
+    assert result.exit_code == 0, result.output
+
+    assert filecmp.cmp(vol_path, target_path, shallow=False)
--- tests/commands/volume/commands_volume_upload_test.py
+++ tests/commands/volume/commands_volume_upload_test.py
@@ -5,10 +5,85 @@
 
 from __future__ import annotations
 
+import filecmp
+import os
+import random
+import sys
+
+from pathlib import Path
+from typing import TYPE_CHECKING, Any
+
 import pytest
 
+from fvirt.cli import cli
 
-@pytest.mark.xfail(reason='Not yet implemented.')
-def test_command_run() -> None:
-    '''Test that the command runs correctly.'''
-    assert False
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from click.testing import CliRunner
+
+    from fvirt.libvirt import Volume
+
+
+@pytest.mark.slow
+@pytest.mark.libvirtd
+def test_volume_upload(tmp_path: Path, cli_runner: CliRunner, live_volume: Volume, unique: Callable[..., Any]) -> None:
+    '''Test volume upload functionality.'''
+    uri = str(live_volume._hv.uri)
+    pool = live_volume._parent
+    vol_path = Path(live_volume.path)
+    target_path = tmp_path / unique('text', prefix='fvirt-test')
+
+    assert pool is not None
+
+    target_path.write_bytes(random.randbytes(live_volume.capacity))
+
+    result = cli_runner.invoke(cli, ('-c', uri, 'volume', 'upload', pool.name, live_volume.name, str(target_path)))
+    assert result.exit_code == 0, result.output
+
+    assert filecmp.cmp(vol_path, target_path, shallow=False)
+
+
+@pytest.mark.slow
+@pytest.mark.libvirtd
+def test_volume_upload_resize(tmp_path: Path, cli_runner: CliRunner, live_volume: Volume, unique: Callable[..., Any]) -> None:
+    '''Test volume upload resize functionality.'''
+    uri = str(live_volume._hv.uri)
+    pool = live_volume._parent
+    vol_path = Path(live_volume.path)
+    target_path = tmp_path / unique('text', prefix='fvirt-test')
+
+    assert pool is not None
+
+    target_path.write_bytes(random.randbytes(live_volume.capacity * 2))
+
+    result = cli_runner.invoke(cli, ('-c', uri, 'volume', 'upload', '--resize', pool.name, live_volume.name, str(target_path)))
+    assert result.exit_code == 0, result.output
+
+    assert filecmp.cmp(vol_path, target_path, shallow=False)
+
+
+@pytest.mark.slow
+@pytest.mark.libvirtd
+@pytest.mark.xfail(condition=sys.platform == 'win32', reason='Sparse data handling not supported on Windows')
+def test_volume_sparse_upload(tmp_path: Path, cli_runner: CliRunner, live_volume: Volume, unique: Callable[..., Any]) -> None:
+    '''Test volume sparse upload functionality.'''
+    uri = str(live_volume._hv.uri)
+    pool = live_volume._parent
+    vol_path = Path(live_volume.path)
+    target_path = tmp_path / unique('text', prefix='fvirt-test')
+
+    assert pool is not None
+
+    block_count = 8
+    block = live_volume.capacity // block_count
+
+    with target_path.open('wb') as f:
+        for k in range(0, block_count // 2):
+            f.seek(block, os.SEEK_CUR)
+            f.write(random.randbytes(block))
+
+    result = cli_runner.invoke(cli, ('-c', uri, 'volume', 'upload', '--sparse', pool.name, live_volume.name, str(target_path)))
+    assert result.exit_code == 0, result.output
+
+    assert filecmp.cmp(vol_path, target_path, shallow=False)
