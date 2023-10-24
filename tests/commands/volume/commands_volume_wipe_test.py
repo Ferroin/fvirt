e1ef6ca4d2d2945413f394cbf24029e616a72eae
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-10-24 11:10:56 -0400

    Add test for volume wipe command and fix issues it exposes.

--- tests/commands/volume/commands_volume_wipe_test.py
+++ tests/commands/volume/commands_volume_wipe_test.py
@@ -5,10 +5,39 @@
 
 from __future__ import annotations
 
+from pathlib import Path
+from typing import TYPE_CHECKING
+
 import pytest
 
+if TYPE_CHECKING:
+    from collections.abc import Callable
 
-@pytest.mark.xfail(reason='Not yet implemented.')
-def test_command_run() -> None:
-    '''Test that the command runs correctly.'''
-    assert False
+    from click.testing import Result
+
+    from fvirt.libvirt import StoragePool, Volume
+
+
+@pytest.mark.libvirtd
+@pytest.mark.slow
+def test_volume_wipe(runner: Callable[..., Result], live_pool: StoragePool, volume_factory: Callable[[StoragePool, int], Volume]) -> None:
+    '''Test that the volume wipe command works correctly.'''
+    uri = str(live_pool._hv.uri)
+    vol = volume_factory(live_pool, 65536)  # Smaller than default intentionally to speed up the test.
+
+    try:
+        path = Path(vol.path)
+        size = vol.capacity
+        data = b'\x00\x55\xAA\xFF' * (size // 4)
+
+        path.write_bytes(data)
+
+        runner(('-c', uri, 'volume', 'wipe', live_pool.name, vol.name), 0)
+
+        assert vol.capacity == size
+
+        wiped_data = path.read_bytes()
+
+        assert wiped_data != data
+    finally:
+        vol.undefine()
