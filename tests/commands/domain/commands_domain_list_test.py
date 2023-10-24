afa1f5f149308e7efce791430573014fb893f959
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-10-24 08:09:42 -0400

    Add tests for list commands.

--- tests/commands/domain/commands_domain_list_test.py
+++ tests/commands/domain/commands_domain_list_test.py
@@ -9,12 +9,17 @@ from typing import TYPE_CHECKING
 
 import pytest
 
+from fvirt.cli import cli
 from fvirt.commands.domain.list import COLUMNS, DEFAULT_COLS
 
-from ..shared import check_columns, check_default_columns
+from ..shared import check_columns, check_default_columns, check_list_entry, check_list_output
 
 if TYPE_CHECKING:
-    from fvirt.libvirt.domain import Domain
+    from collections.abc import Sequence
+
+    from click.testing import CliRunner
+
+    from fvirt.libvirt import Domain, Hypervisor
 
 
 def test_column_definitions(test_dom: Domain) -> None:
@@ -27,7 +32,38 @@ def test_default_columns() -> None:
     check_default_columns(COLUMNS, DEFAULT_COLS)
 
 
-@pytest.mark.xfail(reason='Not yet implemented.')
-def test_command_run() -> None:
-    '''Test that the command runs correctly.'''
-    assert False
+def test_list(cli_runner: CliRunner, test_hv: Hypervisor) -> None:
+    '''Test the list command.'''
+    uri = str(test_hv.uri)
+    dom = test_hv.domains.get(1)
+    assert dom is not None
+
+    result = cli_runner.invoke(cli, ('-c', uri, '--units', 'bytes', 'domain', 'list'))
+    assert result.exit_code == 0
+
+    check_list_output(result.output, dom, tuple(COLUMNS[x] for x in DEFAULT_COLS))
+
+
+def test_no_headings(cli_runner: CliRunner, test_hv: Hypervisor) -> None:
+    '''Test the --no-headings option.'''
+    uri = str(test_hv.uri)
+    dom = test_hv.domains.get(1)
+    assert dom is not None
+
+    result = cli_runner.invoke(cli, ('-c', uri, '--units', 'bytes', 'domain', 'list', '--no-headings'))
+    assert result.exit_code == 0
+
+    check_list_entry(result.output, dom, tuple(COLUMNS[x] for x in DEFAULT_COLS))
+
+
+@pytest.mark.parametrize('a, e', (
+    ('name', ('test',)),
+    ('uuid', ('6695eb01-f6a4-8304-79aa-97f2502e193f',)),
+    ('id', ('1',)),
+))
+def test_list_only(cli_runner: CliRunner, test_uri: str, a: str, e: Sequence[str]) -> None:
+    '''Test that the --only option works correctly.'''
+    result = cli_runner.invoke(cli, ('-c', test_uri, 'domain', 'list', '--only', a))
+    assert result.exit_code == 0
+
+    assert set(e) == set(result.output.splitlines())
--- tests/commands/volume/commands_volume_list_test.py
+++ tests/commands/volume/commands_volume_list_test.py
@@ -7,12 +7,17 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import pytest
+
+from fvirt.cli import cli
 from fvirt.commands.volume.list import COLUMNS, DEFAULT_COLS
 
-from ..shared import check_columns, check_default_columns
+from ..shared import check_columns, check_default_columns, check_list_entry, check_list_output
 
 if TYPE_CHECKING:
-    from fvirt.libvirt.volume import Volume
+    from click.testing import CliRunner
+
+    from fvirt.libvirt import StoragePool, Volume
 
 
 def test_column_definitions(test_volume: Volume) -> None:
@@ -23,3 +28,55 @@ def test_column_definitions(test_volume: Volume) -> None:
 def test_default_columns() -> None:
     '''Test that the default column list is valid.'''
     check_default_columns(COLUMNS, DEFAULT_COLS)
+
+
+@pytest.mark.libvirtd
+def test_list(cli_runner: CliRunner, live_volume: Volume) -> None:
+    '''Test the list command.'''
+    uri = str(live_volume._hv.uri)
+    pool = live_volume._parent
+    assert pool is not None
+
+    result = cli_runner.invoke(cli, ('-c', uri, '--units', 'bytes', 'volume', 'list', pool.name))
+    assert result.exit_code == 0
+
+    check_list_output(result.output, live_volume, tuple(COLUMNS[x] for x in DEFAULT_COLS))
+
+
+@pytest.mark.libvirtd
+def test_no_headings(cli_runner: CliRunner, live_volume: Volume) -> None:
+    '''Test the --no-headings option.'''
+    uri = str(live_volume._hv.uri)
+    pool = live_volume._parent
+    assert pool is not None
+
+    result = cli_runner.invoke(cli, ('-c', uri, '--units', 'bytes', 'volume', 'list', '--no-headings', pool.name))
+    assert result.exit_code == 0
+
+    check_list_entry(result.output, live_volume, tuple(COLUMNS[x] for x in DEFAULT_COLS))
+
+
+@pytest.mark.libvirtd
+def test_list_only_name(cli_runner: CliRunner, live_volume: Volume) -> None:
+    '''Test listing only names.'''
+    uri = str(live_volume._hv.uri)
+    pool = live_volume._parent
+    assert pool is not None
+
+    result = cli_runner.invoke(cli, ('-c', uri, 'volume', 'list', '--only', 'name', pool.name))
+    assert result.exit_code == 0
+
+    assert result.output.rstrip() == str(live_volume.name)
+
+
+@pytest.mark.libvirtd
+def test_list_only_key(cli_runner: CliRunner, live_volume: Volume) -> None:
+    '''Test listing only keys.'''
+    uri = str(live_volume._hv.uri)
+    pool = live_volume._parent
+    assert pool is not None
+
+    result = cli_runner.invoke(cli, ('-c', uri, 'volume', 'list', '--only', 'key', pool.name))
+    assert result.exit_code == 0
+
+    assert result.output.rstrip() == str(live_volume.key)
