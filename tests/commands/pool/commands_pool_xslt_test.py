ac85eb4be34251925ce970513cea68eb617874f8
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-10-26 11:40:30 -0400

    Add pool xslt command test.

--- tests/commands/pool/commands_pool_xslt_test.py
+++ tests/commands/pool/commands_pool_xslt_test.py
@@ -5,10 +5,37 @@
 
 from __future__ import annotations
 
-import pytest
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Callable, Sequence
+    from pathlib import Path
+
+    from click.testing import Result
+
+    from fvirt.libvirt import StoragePool
 
 
-@pytest.mark.xfail(reason='Not yet implemented.')
-def test_command_run() -> None:
+def test_command_run(
+    runner: Callable[[Sequence[str], int], Result],
+    live_pool: StoragePool,
+    xslt_doc_factory: Callable[[str, str], str],
+    tmp_path: Path,
+) -> None:
     '''Test that the command runs correctly.'''
-    assert False
+    uri = str(live_pool._hv.uri)
+    xslt_path = tmp_path / 'transform.xslt'
+
+    e = live_pool.config.find('/target/path')
+    assert e is not None
+
+    xslt_path.write_text(xslt_doc_factory('target/path', '/test'))
+
+    result = runner(('-c', uri, 'pool', 'xslt', live_pool.name, str(xslt_path)), 0)
+
+    assert len(result.output) > 0
+
+    e = live_pool.config.find('/target/path')
+    assert e is not None
+
+    assert e.text == '/test'
