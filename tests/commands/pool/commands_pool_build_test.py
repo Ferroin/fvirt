462e1edf3135e3c0aeb2615fda2394a2ae593af1
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-10-27 08:47:56 -0400

    Add test for pool build command.

--- tests/commands/pool/commands_pool_build_test.py
+++ tests/commands/pool/commands_pool_build_test.py
@@ -5,13 +5,35 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import pytest
 
+if TYPE_CHECKING:
+    from collections.abc import Callable, Sequence
+    from contextlib import _GeneratorContextManager
 
-@pytest.mark.xfail(reason='Test not yet implemented.')
-def test_command_run() -> None:
+    from click.testing import Result
+
+    from fvirt.libvirt import Hypervisor
+
+
+def test_command_run(
+    runner: Callable[[Sequence[str], int], Result],
+    live_hv: Hypervisor,
+    pool_xml: Callable[[], str],
+    serial: Callable[[str], _GeneratorContextManager[None]],
+) -> None:
     '''Test that the command runs correctly.'''
-    assert False
+    # TODO: Needs a better test case that actually confirms the pool was built.
+    with serial('live-pool'):
+        pool = live_hv.define_storage_pool(pool_xml())
+
+    result = runner(('-c', str(live_hv.uri), 'pool', 'build', pool.name), 0)
+    assert len(result.output) > 0
+
+    with serial('live-pool'):
+        pool.undefine()
 
 
 @pytest.mark.xfail(reason='Test not yet implemented')
