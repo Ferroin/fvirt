fb93e649f46f4e602a1bac8e1be6f28b63b7dcb2
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-10-27 22:16:41 -0400

    Add more pool command tests.

--- tests/commands/pool/commands_pool_define_test.py
+++ tests/commands/pool/commands_pool_define_test.py
@@ -5,16 +5,85 @@
 
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
 import pytest
 
+from lxml import etree
 
-@pytest.mark.xfail(reason='Test not yet implemented.')
-def test_command_run() -> None:
+if TYPE_CHECKING:
+    from collections.abc import Callable, Sequence
+    from contextlib import _GeneratorContextManager
+    from pathlib import Path
+
+    from click.testing import Result
+
+    from fvirt.libvirt import Hypervisor
+
+
+@pytest.mark.libvirtd
+def test_command_run(
+    runner: Callable[[Sequence[str], int], Result],
+    live_hv: Hypervisor,
+    pool_xml: Callable[[], str],
+    tmp_path: Path,
+    serial: Callable[[str], _GeneratorContextManager],
+) -> None:
     '''Test that the command runs correctly.'''
-    assert False
+    uri = str(live_hv.uri)
+    conf_path = tmp_path / 'pool.xml'
+    xml = pool_xml()
+    conf_path.write_text(xml)
+    e = etree.XML(xml).find('./name')
+    assert e is not None
+    name = e.text
+    assert name is not None
+
+    with serial('live-pool'):
+        result = runner(('-c', uri, 'pool', 'define', str(conf_path)), 0)
+
+    assert len(result.output) > 0
+
+    pool = live_hv.storage_pools.get(name)
+
+    assert pool is not None
+    assert not pool.running
 
 
-@pytest.mark.xfail(reason='Test not yet implemented')
-def test_command_bulk_run() -> None:
+@pytest.mark.libvirtd
+def test_command_bulk_run(
+    runner: Callable[[Sequence[str], int], Result],
+    live_hv: Hypervisor,
+    pool_xml: Callable[[], str],
+    tmp_path: Path,
+    serial: Callable[[str], _GeneratorContextManager],
+) -> None:
     '''Test running the command on multiple objects.'''
-    assert False
+    uri = str(live_hv.uri)
+    count = 3
+    xmls = tuple(
+        pool_xml() for _ in range(0, count)
+    )
+    names = []
+    paths = []
+
+    for i, x in enumerate(xmls):
+        e = etree.XML(x).find('./name')
+        assert e is not None
+        assert e.text is not None
+        names.append(e.text)
+
+        p = tmp_path / f'pool{i}.xml'
+        p.write_text(x)
+        paths.append(str(p))
+
+    with serial('live-pool'):
+        result = runner(('-c', uri, 'pool', 'define') + tuple(paths), 0)
+
+    assert len(result.output) > 0
+
+    for name in names:
+        pool = live_hv.storage_pools.get(name)
+
+        assert pool is not None
+        assert not pool.running
