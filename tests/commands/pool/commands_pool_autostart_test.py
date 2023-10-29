baa53b6240ed656d008400a79c01da018a0ae6fd
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-10-28 22:20:11 -0400

    Fix up autostart commands and test pool autostart fully.

--- tests/commands/pool/commands_pool_autostart_test.py
+++ tests/commands/pool/commands_pool_autostart_test.py
@@ -11,12 +11,14 @@ import pytest
 
 if TYPE_CHECKING:
     from collections.abc import Callable, Sequence
+    from contextlib import _GeneratorContextManager
 
     from click.testing import Result
 
     from fvirt.libvirt import Hypervisor, StoragePool
 
 
+@pytest.mark.libvirtd
 def test_command_run(
     runner: Callable[[Sequence[str], int], Result],
     live_pool: tuple[StoragePool, Hypervisor],
@@ -27,38 +29,41 @@ def test_command_run(
 
     assert pool.autostart == False  # noqa: E712
 
-    result = runner(('-c', uri, 'pool', 'autostart', pool.name, 'yes'), 0)
+    result = runner(('-c', uri, 'pool', 'autostart', '--enable', pool.name), 0)
     assert len(result.output) > 0
 
     assert pool.autostart == True  # noqa: E712
 
-    result = runner(('-c', uri, 'pool', 'autostart', pool.name, 'no'), 0)
-    assert len(result.output) > 0
-
-    assert pool.autostart == False  # noqa: E712
-
-    result = runner(('-c', uri, 'pool', 'autostart', pool.name, '1'), 0)
-    assert len(result.output) > 0
-
-    assert pool.autostart == True  # noqa: E712
-
-    result = runner(('-c', uri, 'pool', 'autostart', pool.name, '0'), 0)
-    assert len(result.output) > 0
-
-    assert pool.autostart == False  # noqa: E712
-
-    result = runner(('-c', uri, 'pool', 'autostart', pool.name, 'true'), 0)
-    assert len(result.output) > 0
-
-    assert pool.autostart == True  # noqa: E712
-
-    result = runner(('-c', uri, 'pool', 'autostart', pool.name, 'false'), 0)
+    result = runner(('-c', uri, 'pool', 'autostart', '--disable', pool.name), 0)
     assert len(result.output) > 0
 
     assert pool.autostart == False  # noqa: E712
 
 
-@pytest.mark.xfail(reason='Not yet implemented')
-def test_command_run_bulk() -> None:
+@pytest.mark.libvirtd
+def test_command_run_bulk(
+    runner: Callable[[Sequence[str], int], Result],
+    live_hv: Hypervisor,
+    pool_xml: Callable[[], str],
+    serial: Callable[[str], _GeneratorContextManager[None]],
+    worker_id: str,
+) -> None:
     '''Test that running the command on multiple objects works.'''
-    assert False
+    uri = str(live_hv.uri)
+    count = 3
+
+    with serial('live-pool'):
+        pools = tuple(
+            live_hv.define_storage_pool(pool_xml()) for _ in range(0, count)
+        )
+
+    assert all((not p.autostart) for p in pools)
+
+    result = runner(('-c', uri, 'pool', 'autostart', '--enable', '--match', 'name', f'fvirt-test-{worker_id}-'), 0)
+    assert len(result.output) > 0
+
+    assert all(p.autostart for p in pools)
+
+    with serial('live-pool'):
+        for p in pools:
+            p.undefine()
