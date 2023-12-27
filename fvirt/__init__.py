b653894483f6ba646c35c563c229d1648a6767bb
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-12-27 08:06:43 -0500

    Fix up handling of package version number.
    Provide __version__ at top level to better interface with other tools,
    and derive VERSION from that.

--- fvirt/__init__.py
+++ fvirt/__init__.py
@@ -5,10 +5,16 @@
 
 import sys
 
-from .version import VERSION
+from typing import Final
+
+from .version import VersionNumber
 
 if not sys.version_info >= (3, 11):  # pragma: no cover # By definition this can never be covered by pytest
     raise RuntimeError('fvirt requires Python 3.11 or newer.')
+
+__version__: Final = '0.0.1'
+
+VERSION: Final = VersionNumber.from_string(__version__)
 
 __all__ = [
     'VERSION',
