1d541d08e6f2df0d9bce31a6a36b8923dcbc9c73
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2023-12-28 10:25:36 -0500

    Add configuration file for overriding default runtime config.
    This allows more easy customization by users.

--- fvirt/commands/_base/state.py
+++ fvirt/commands/_base/state.py
@@ -5,17 +5,35 @@
 
 from __future__ import annotations
 
+import logging
 import logging.config
+import os
 import sys
 import threading
 
 from concurrent.futures import ThreadPoolExecutor
-from typing import Self
+from pathlib import Path
+from typing import Final, Self, TypeVar, cast
 
+from .config import FVirtConfig, get_config
 from ...libvirt import URI, Hypervisor
 from ...libvirt.events import start_libvirt_event_thread
 from ...util.dummy_pool import DummyExecutor
 from ...util.units import bytes_to_unit, count_integer_digits
+
+T = TypeVar('T')
+
+LOGGER: Final = logging.getLogger(__name__)
+DEFAULT_MAX_JOBS: Final = 8
+NCPUS: Final = os.cpu_count()
+
+if NCPUS is not None:
+    DEFAULT_JOB_COUNT = min(DEFAULT_MAX_JOBS, NCPUS + 4)
+else:
+    DEFAULT_JOB_COUNT = DEFAULT_MAX_JOBS
+
+if hasattr(os, 'sched_getaffinity') and os.sched_getaffinity(0):
+    DEFAULT_JOB_COUNT = min(DEFAULT_MAX_JOBS, len(os.sched_getaffinity(0)) + 4)
 
 
 def _configure_logging(level: str) -> None:
@@ -51,9 +69,24 @@ def _configure_logging(level: str) -> None:
     })
 
 
+def _effective(config: FVirtConfig, uri: URI, key: str, override: T | None, default: T) -> T:
+    if override is not None:
+        return override
+
+    if str(uri) in config.uris:
+        if (v := getattr(config.uris[str(uri)], key, None)) is not None:
+            return cast(T, v)
+
+    if (v := getattr(config.defaults, key, None)) is not None:
+        return cast(T, v)
+
+    return default
+
+
 class State:
     '''Class representing the internal shared state of the fvirt CLI.'''
     __slots__ = [
+        '__config',
         '__fail_fast',
         '__fail_if_no_match',
         '__hypervisor',
@@ -67,32 +100,58 @@ class State:
 
     def __init__(
         self: Self,
+        config_file: Path | None,
         uri: URI,
-        fail_fast: bool,
-        idempotent: bool,
-        fail_if_no_match: bool,
-        units: str,
-        jobs: int,
-        log_level: str,
+        fail_fast: bool | None,
+        idempotent: bool | None,
+        fail_if_no_match: bool | None,
+        units: str | None,
+        jobs: int | None,
+        log_level: str | None,
     ) -> None:
-        if jobs < 1:
+        if jobs == 0:
+            jobs = DEFAULT_JOB_COUNT
+
+        if isinstance(jobs, int) and jobs < 1:
             raise ValueError('Number of jobs must be at least 1')
 
-        self.__fail_fast = fail_fast
-        self.__fail_if_no_match = fail_if_no_match
-        self.__hypervisor: Hypervisor | None = None
-        self.__idempotent = idempotent
-        self.__jobs = jobs
-        self.__pool: ThreadPoolExecutor | DummyExecutor | None = None
-        self.__thread: threading.Thread | None = None
-        self.__units = units
-        self.__uri = uri
+        self.__config = get_config(config_file)
+
+        if log_level is None:
+            log_level = self.config.log.level
 
         _configure_logging(log_level)
+
+        self.__uri = uri
+        LOGGER.info(f'Using libvirt URI: {uri}')
+
+        self.__fail_fast = _effective(self.config, self.uri, 'fail_fast', fail_fast, False)
+        LOGGER.info(f'Fail-fast: {"enabled" if self.fail_fast else "disabled"}')
+
+        self.__fail_if_no_match = _effective(self.config, self.uri, 'fail_if_no_match', fail_if_no_match, False)
+        LOGGER.info(f'Fail-if-no-match: {"enabled" if self.fail_if_no_match else "disabled"}')
+
+        self.__idempotent = _effective(self.config, self.uri, 'idempotent', idempotent, True)
+        LOGGER.info(f'Idempotent: {"enabled" if self.idempotent else "disabled"}')
+
+        self.__jobs = _effective(self.config, self.uri, 'jobs', jobs, DEFAULT_JOB_COUNT)
+        LOGGER.debug(f'Jobs: {self.jobs}')
+
+        self.__units = _effective(self.config, self.uri, 'units', units, 'si')
+        LOGGER.debug(f'Units: {self.__units}')
+
+        self.__hypervisor: Hypervisor | None = None
+        self.__pool: ThreadPoolExecutor | DummyExecutor | None = None
+        self.__thread: threading.Thread | None = None
 
     def __del__(self: Self) -> None:
         if self.__pool is not None:
             self.__pool.shutdown(wait=True, cancel_futures=True)
+
+    @property
+    def config(self: Self) -> FVirtConfig:
+        '''The configuration for this instance of fvirt.'''
+        return self.__config
 
     @property
     def uri(self: Self) -> URI:
@@ -135,8 +194,10 @@ class State:
         '''The thread pool to use for concurrent operations.'''
         if self.__pool is None:
             if self.jobs == 1:
+                LOGGER.debug('Creating fake thread pool for serialized execution.')
                 self.__pool = DummyExecutor()
             else:
+                LOGGER.info(f'Starting thread pool with {self.jobs} threads.')
                 self.__pool = ThreadPoolExecutor(
                     max_workers=self.jobs,
                     thread_name_prefix='fvirt-worker',
