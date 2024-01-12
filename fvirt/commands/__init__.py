f6974e6c28e3c94e0771d792b97683a69181565b
Austin S. Hemmelgarn <ahferroin7@gmail.com>
2024-01-12 09:41:14 -0500

    Add commands to dump configuration.

--- fvirt/commands/__init__.py
+++ fvirt/commands/__init__.py
@@ -8,6 +8,7 @@ from __future__ import annotations
 from typing import Final
 
 LAZY_COMMANDS: Final = {
+    'config': 'fvirt.commands.config.config',
     'domain': 'fvirt.commands.domain.domain',
     'host': 'fvirt.commands.host.host',
     'pool': 'fvirt.commands.pool.pool',
